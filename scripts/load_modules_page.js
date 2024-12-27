f76acf8171183bb556b6df6551337cbe3b47346a
NoLifeSMP <nicolasmatasoff@gmail.com>
2024-12-27 00:07:38 -0500

    oops typo

--- scripts/load_modules_page.js
+++ scripts/load_modules_page.js
@@ -1,3 +1,3 @@
-import { modData, generatePage } from "https://realiste1.github.io/TP5-InterfaceWeb/modules/load_mod_page.js";
+import { modData, generatePage } from "https://realiste1.github.io/TP5-InterfaceWeb/scripts/modules/load_mod_page.js";
 window.modData = modData;
 window.generatePage = generatePage; /* Utiliser dans recherche.html */
